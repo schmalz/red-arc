0e7b1500934d9cd41601d05ea7d9a654bc97bd02
schmalz <schmalz@live.co.uk>
2021-07-20 16:52:10 +0100

    Initial commit.

---
+++ src/sketch/dynamic.clj
@@ -0,0 +1,81 @@
+(ns sketch.dynamic
+  (:require [clojure.pprint :as pretty]
+            [quil.core :as quil]))
+
+(def colours {:base03 [193 100 21]
+              :base02 [192 90 26]
+              :base01 [194 25 46]
+              :base00 [195 23 51]
+              :base0 [186 13 59]
+              :base1 [180 9 63]
+              :base2 [44 11 93]
+              :base3 [44 10 99]
+
+              :yellow [45 100 71]
+              :orange [8 89 80]
+              :red [1 79 86]
+              :magenta [331 74 83]
+              :violet [237 45 77]
+              :blue [205 82 82]
+              :cyan [175 74 63]
+              :green [68 100 60]
+              
+              :black [0 0 0]})
+
+(defn save-frame-to-disk
+  ([]
+   (quil/save-frame (pretty/cl-format nil
+                                      "frames/~d-~2,'0d-~2,'0d-~2,'0d-~2,'0d-~2,'0d-####.jpeg"
+                                      (quil/year) (quil/month) (quil/day)
+                                      (quil/hour) (quil/minute) (quil/seconds))))
+  ([state _]
+   (save-frame-to-disk)
+   state))
+
+(defn- stroke
+  [colour]
+  (quil/stroke 1 79 (rand 100) (rand)#_(flatten (conj (colours colour) (rand)))))
+
+(defn- background
+  [colour]
+  (apply quil/background (colours colour)))
+
+(defn- arc
+  []
+  (let [d (quil/random (* (quil/width)
+                          2))
+        j 5
+        x (quil/random (- j)
+                       j)
+        y (quil/random (- j)
+                       j)]
+    (stroke :red)
+    (quil/arc x
+              y
+              d
+              d
+              (quil/random quil/TWO-PI)
+              (quil/random quil/TWO-PI))))
+
+(defn- arcs
+  [n]
+  #_(stroke :red)
+  (quil/with-translation [(/ (quil/width)
+                             2)
+                          (/ (quil/height)
+                             2)]
+    (dotimes [_ n]
+      (arc))))
+
+(defn draw
+  []
+  (quil/no-loop)
+  (quil/no-fill)
+  (background :black)
+  (arcs 101)
+  (save-frame-to-disk))
+
+(defn initialise
+  []
+  (quil/smooth)
+  (quil/color-mode :hsb 360 100 100 1.0))
